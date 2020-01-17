b46a33e3db843b8d4e28fadca85797065a9323e9
tomatist <lindts.wr@gmail.com>
2020-01-18 04:00:39 +0900

    Mの指摘の修正
    ・３行目のtotalという変数を削除
    ・テンプレートリテラルを使って８、９行目を一つの文にした。
    ・全体的に文の最後の「;」を削除

--- beginner_20200116.js
+++ beginner_20200116.js
@@ -1,16 +1,15 @@
 // fs(file system)というライブラリーをロードする。
 const fs = require('fs')
 
-let total = 0; // totalの初期値は0
 // for文の実行フロー
 // iを0で初期化
 // iが１〜1000以下（条件式を満たす）ならfor文の処理を実行
 // iに1を足し、再び条件式の判定へ
 for (let i = 0; i < 1000; i++) {
-    total = i + 2; // 1から1000の値をtotalに加算している
-    if (total % 2 === 0){ //偶数のみ表示。
-        console.log(total);　//結果を表示。
-        fs.appendFileSync('test_file.txt', total) // もしtest_file.txtというファイルが存在した場合、ファイルが上書きされる。
+    let = i + 2; // 1から1000の値をtotalに加算している
+    if (let % 2 === 0){ //偶数のみ表示。
+        console.log(let);　//結果を表示。
+        fs.appendFileSync('test_file.txt', let) // もしtest_file.txtというファイルが存在した場合、ファイルが上書きされる。
         fs.appendFileSync('test_file.txt', '\n') // もしtest_file.txtというファイルが存在した場合、ファイルが上書きされる。
     }
 }
